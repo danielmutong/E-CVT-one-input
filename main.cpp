76e4229e27fdb942bc67ff86df585de5b5cd37d5
Daniel Wang <danielmutong@yahoo.ca>
2021-01-15 21:11:45 -0800

    update

--- main.cpp
+++ main.cpp
@@ -11,32 +11,33 @@
 #include "pid.hpp"
 #define ROWS 21
 #define COLS 7
-#define MAX_KEY1 2501
-#define MAX_KEY2 101
+#define MIN_KEY1 1999
+#define MAX_KEY2 3601
 using namespace std;
 
 
 
 int main(void){
     pid test;
+    int rpm = 0;
+    int measured_sheath = 0;
+    int ideal_sheath = 0;
+    /*
+    while(rpm < 2000){
+        rpm = getRPM();
+    }
+     */
+    while(rpm < 3550){
+        rpm = getRPM();
+        LookUp a(rpm);
+        ideal_sheath = a.findkey();
+        measured_sheath = getSheath();
+        test.pid_task(ideal_sheath, measured_sheath);
+        
+    }
     while(1){
-        float key1 = 0;
-        float key2 = 0;
-        float value = 0;
-        float sensor_value = 0;
-        key1 = getRPM();
-        key2 = getThrottle();
-        cout << "key 1 is " << key1 << " and key 2 is " << key2 << endl;
-        if((key1 < 0) || (key1 > MAX_KEY1) || (key2 < 0) || (key2 > MAX_KEY2))
-            cout << "key out of range" << endl;
-        else{
-            LookUp a(key1, key2);
-            value = a.findkey();
-            cout << "output is: " << value << endl;
-            sensor_value = getTorque();
-            test.pid_task(value, sensor_value);
-
-        }
+        rpm = getRPM();
+        test.pid_task(3600, rpm);
     }
 
     
